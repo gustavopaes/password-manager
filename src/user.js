aeecc721f659a80269e6e92a233236e0430dd0bd
Gustavo Paes <gustavo.paes@gmail.com>
2017-06-26 23:55:47 +0000

    Define array de serviços para evitar quebras

--- src/user.js
+++ src/user.js
@@ -78,7 +78,9 @@ class User {
     user.login = login;
     user.dataPath = '';
     user.token = crypto.randomBytes(32).toString('hex');
-    user.data = {};
+    user.data = {
+      services: []
+    };
 
     return new Promise((resolve, reject) => {
       // valida se o usuário existe
